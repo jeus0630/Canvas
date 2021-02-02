6b3c471a474acfa04aa6120c1126c7027a00e506
jeus0630 <jeus0630@designfever.com>
2021-02-02 19:08:11 +0900

    색상 추가

--- canvas/main.js
+++ canvas/main.js
@@ -1,48 +1,16 @@
-$(document).ready(function(){
-    const canvas = document.querySelector('canvas');
-    const ctx = canvas.getContext('2d');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-    let arr = [];
+let colors = ['#F23041','#8C2639','#2C3E40','#78BFB2','#F21313'];
+let arr = [];
+const mouse = {
+    x : undefined,
+    y : undefined
+}
 
-    init();
-    draw();
-    move();
-
-    function init(){
-        canvas.width = innerWidth;
-        canvas.height = innerHeight;
-    }
-
-    function draw(){
-        for(let i = 0; i < 100; i++){
-            let x = Math.random()*innerWidth;
-            let y = Math.random()*innerHeight;
-            const radius = Math.random()*100;
-            if(x + radius > innerWidth) x -= radius;
-            if(x - radius < 0 ) x += radius;
-            if(y + radius > innerHeight) y -= radius;
-            if(y - radius < 0) y += radius;
-
-            arr.push(new Canvas(x,y,Math.random()*10, Math.random()*10,radius));
-        }
-    }
-
-    function move(){
-
-        loop();
-
-        function loop(){
-            ctx.clearRect(0,0,innerWidth,innerHeight);
-
-            arr.forEach(function(el,index){
-                el.draw();
-            })
-
-            requestAnimationFrame(loop);
-        }
-
-    }
-
+$(window).on('mousemove',function(e){
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
 })
 
 function Canvas(x,y,dx,dy,radius){
@@ -53,15 +21,17 @@ function Canvas(x,y,dx,dy,radius){
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.randomNum = Math.floor(Math.random() * colors.length);
 
     this.draw();
 }
 
 Canvas.prototype.draw = function(){
+
     this.ctx.beginPath();
-    this.ctx.strokeStyle = '#2d185c';
     this.ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
-    this.ctx.stroke();
+    this.ctx.fillStyle = colors[this.randomNum];
+    this.ctx.fill();
 
     if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
         this.dx = -this.dx;
@@ -73,5 +43,48 @@ Canvas.prototype.draw = function(){
 
     this.x += this.dx;
     this.y += this.dy;
+
+    if(mouse.x - this.x < 100 && mouse.x - this.x > -100 && mouse.y - this.y < 100 && mouse.y - this.y > -100){
+        this.radius++;
+    }
+
+}
+
+init();
+draw();
+move();
+
+function init(){
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+}
+
+function draw(){
+    for(let i = 0; i < 200; i++){
+        let x = Math.random()*innerWidth;
+        let y = Math.random()*innerHeight;
+        const radius = Math.random()*15;
+        if(x + radius > innerWidth) x -= radius;
+        if(x - radius < 0 ) x += radius;
+        if(y + radius > innerHeight) y -= radius;
+        if(y - radius < 0) y += radius;
+
+        arr.push(new Canvas(x,y,Math.random()*10, Math.random()*10,radius));
+    }
+}
+
+function move(){
+
+    loop();
+
+    function loop(){
+        ctx.clearRect(0,0,innerWidth,innerHeight);
+
+        arr.forEach(function(el,index){
+            el.draw();
+        })
+
+        requestAnimationFrame(loop);
+    }
 
 }
