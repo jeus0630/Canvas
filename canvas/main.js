1d3946d22e18827b8c7961189276265b4b165bac
jeus0630 <jeus0630@designfever.com>
2021-02-03 13:58:08 +0900

    마우스 무브 이벤트 추가

--- canvas/main.js
+++ canvas/main.js
@@ -1,90 +1,92 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-
-let colors = ['#F23041','#8C2639','#2C3E40','#78BFB2','#F21313'];
-let arr = [];
-const mouse = {
-    x : undefined,
-    y : undefined
+function Canvas(x,y,dx,dy,radius){
+    this.init(x,y,dx,dy,radius);
+    this.draw();
 }
 
-$(window).on('mousemove',function(e){
-    mouse.x = e.pageX;
-    mouse.y = e.pageY;
-})
-
-function Canvas(x,y,dx,dy,radius){
-    this.canvas = document.querySelector('canvas');
-    this.ctx = this.canvas.getContext('2d');
+Canvas.prototype.init = function(x,y,dx,dy,radius){
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
-    this.randomNum = Math.floor(Math.random() * colors.length);
+    this.minRadius = this.radius;
+    this.maxRadius = 60;
+    this.color = colors[Math.floor( Math.random() * colors.length)];
 
-    this.draw();
+    if(Math.floor(this.dx) % 2 == 0) this.dx = -this.dx;
+    if(Math.floor(this.dy) % 2 == 0) this.dy = -this.dy;
+
+    this.canvas = document.querySelector('canvas');
+    this.ctx = canvas.getContext('2d');
 }
 
 Canvas.prototype.draw = function(){
-
     this.ctx.beginPath();
     this.ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
-    this.ctx.fillStyle = colors[this.randomNum];
+    this.ctx.fillStyle = this.color;
     this.ctx.fill();
-
-    if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
-        this.dx = -this.dx;
-    }
-
-    if(this.y + this.radius > innerHeight || this.y - this.radius < 0 ){
-        this.dy = -this.dy;
-    }
 
     this.x += this.dx;
     this.y += this.dy;
 
-    if(mouse.x - this.x < 100 && mouse.x - this.x > -100 && mouse.y - this.y < 100 && mouse.y - this.y > -100){
-        this.radius++;
+    if(this.x + this.dx > innerWidth || this.x + this.dx < 0) this.dx = -this.dx;
+    if(this.y + this.dy > innerHeight || this.y + this.dy < 0) this.dy = -this.dy;
+
+    if(this.x - mouse.x < 100 && this.x - mouse.x > -100 && this.y - mouse.y < 100 && this.y - mouse.y > -100){
+        if(this.radius < this.maxRadius) this.radius += 2;
+    }else if(this.radius > this.minRadius){
+        this.radius -= 2;
     }
-
 }
 
-init();
-draw();
-move();
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-function init(){
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
+let arr = [];
+let colors = ['#020659','#010440','#010326','#C0D904','#888C03'];
+let mouse = {
+    x : undefined,
+    y : undefined
 }
 
-function draw(){
-    for(let i = 0; i < 200; i++){
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+loop();
+createCircles();
+
+function loop(){
+    ctx.clearRect(0,0,innerWidth, innerHeight);
+
+    arr.forEach(function(el,index){
+        el.draw();
+    })
+
+    requestAnimationFrame(loop);
+}
+
+function createCircles(){
+
+    for(let i = 0; i < 500; i++){
         let x = Math.random()*innerWidth;
-        let y = Math.random()*innerHeight;
-        const radius = Math.random()*15;
+        let y= Math.random()*innerHeight;
+        const dx = Math.random()*5;
+        const dy = Math.random()*5;
+        const radius = Math.random()*10 + 2;
+
         if(x + radius > innerWidth) x -= radius;
-        if(x - radius < 0 ) x += radius;
+        if(x - radius < 0) x += radius;
         if(y + radius > innerHeight) y -= radius;
         if(y - radius < 0) y += radius;
 
-        arr.push(new Canvas(x,y,Math.random()*10, Math.random()*10,radius));
-    }
-}
-
-function move(){
-
-    loop();
-
-    function loop(){
-        ctx.clearRect(0,0,innerWidth,innerHeight);
-
-        arr.forEach(function(el,index){
-            el.draw();
-        })
-
-        requestAnimationFrame(loop);
+        arr.push(new Canvas(x,y,dx,dy,radius));
     }
 
 }
+
+$(window).on({
+    mousemove : function(e){
+        mouse.x = e.pageX;
+        mouse.y = e.pageY;
+    }
+});
